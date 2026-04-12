3254a7bb7f77bd6a84d5aede19c2c9d9af976355
Ebrahim Al-Serri <ebrahim.al-serri@georgebrown.ca>
2026-04-11 22:38:43 -0400

    Set up routing and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { EmployeeList } from './employee-list/employee-list';
+import { AddEmployee } from './add-employee/add-employee';
+import { EmployeeDetails } from './employee-details/employee-details';
+import { UpdateEmployee } from './update-employee/update-employee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+    { path: 'login', component: Login },
+    { path: 'signup', component: Signup },
+    
+    { path: 'employees', component: EmployeeList },
+    { path: 'employees/add', component: AddEmployee },
+    { path: 'employees/:id', component: EmployeeDetails },
+    { path: 'employees/edit/:id', component: UpdateEmployee },
+];
